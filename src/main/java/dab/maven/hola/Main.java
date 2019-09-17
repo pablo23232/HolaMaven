2c7a2577c5bb6c85b86b8d520936d71e6c2c5fbc
Pablo <pablo23232@gmail.com>
2019-09-17 12:26:21 +0100

    first commit

---
+++ src/main/java/dab/maven/hola/Main.java
@@ -0,0 +1,14 @@
+package dab.maven.hola;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hola");
+		String greetings = org.apache.commons.lang3.StringUtils.reverse(("Hola Maven"));
+		System.out.println(greetings);
+
+	}
+
+}
